f535eabbe2ea7f1f76f70bcfb5e9915d0f356400
LiashenkoMA <makcim_an@mail.ru>
2025-08-07 20:46:36 +0400

    feat: редактор статей в админ панели

--- src/app/_components/ArticleDownloadForm/ArticleDownloadForm.tsx
+++ src/app/_components/ArticleDownloadForm/ArticleDownloadForm.tsx
@@ -1,9 +1,30 @@
-import "./articleDownloadForm.scss";
+import dynamic from "next/dynamic";
+import { useState } from "react";
+
+const EditorComp = dynamic(
+  () => import("@/app/_components/MDXEditor/MdxEditor"),
+  { ssr: false }
+);
 
 export default function ArticleDownloadForm() {
+  const markdown = `
+Hello **world**!
+`;
+  const [article, setArticle] = useState("");
+
+  async function handleSubmin(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    console.log(article);
+  }
+
   return (
-    <div>
-      <h2>ArticleDownloadForm</h2>
-    </div>
+    <form onSubmit={handleSubmin}>
+      <EditorComp
+        markdown={markdown}
+        onChange={(value: string) => setArticle(value)}
+      />
+      ;<button type="submit">Опубликовать статью</button>
+    </form>
   );
 }

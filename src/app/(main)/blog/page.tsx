381f0f22089630643c69f65d9d6fd627a8ddf10b
LiashenkoMA <makcim_an@mail.ru>
2025-09-10 18:03:52 +0400

    feat: страница блог

--- src/app/(main)/blog/page.tsx
+++ src/app/(main)/blog/page.tsx
@@ -1,15 +1,37 @@
-export default async function Page({
-  params,
-}: {
-  params: { category: string; article: string };
-}) {
+import "./blog.scss";
 
-  console.log(params)
+import { getArticles } from "@/app/_utils/articleApi";
+import { getUserData } from "@/app/_utils/userApi";
+import PageCard from "@/app/_components/PageCard/PageCard";
+import PageHeader from "@/app/_components/PageHeader/PageHeader";
+import Sidebar from "@/app/_components/Sidebar/Sidebar";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Все статьи моего блога!",
+  description:
+    "На этой странице вы можете найти все доступные на данный момент статьи моего блога. Надеюсь они вам понравятся.",
+};
+
+export default async function Page() {
+  const [articles, user] = await Promise.all([getArticles(), getUserData()]);
 
   return (
-    <div>
-      <h2>blog</h2>
+    <div className="blog">
+      <PageHeader
+        img={user.avatarLink}
+        alt="Моя аватарка"
+        title="Статьи моего блога"
+      />
+      <div className="blog__container">
+        <main className="blog__content">
+          {articles.map((article) => (
+            <PageCard key={article._id} page={article} />
+          ))}
+        </main>
+        <Sidebar />
+      </div>
     </div>
   );
 }
+

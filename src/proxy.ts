5fde35d3c36dd59cce3a94c3eb2ddc4ac5c37c0a
LiashenkoMA <makcim_an@mail.ru>
2025-11-16 17:26:17 +0400

    update до 16 версии nextjs

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 const protectedRoutes = ["/dashboard"];
 const publicRoutes = ["/login"];
 
-export async function middleware(request: NextRequest) {
+export default async function proxy(request: NextRequest) {
   const path = request.nextUrl.pathname;
   const isProtectedRoute = protectedRoutes.includes(path);
   const isPublicRoute = publicRoutes.includes(path);
